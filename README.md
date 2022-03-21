09127681a2a26742b1cda3e9b0f379c93c0e4ee2
DwQ7 <Qindw1004@@163.com>
2022-03-22 00:32:39 +0800

    Vue学习-Router

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # webapp-vue-test
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+**Vue学习-Router：路由+组件，路由的两种模式history&hash**
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <nav>
+    <router-link tag="button" to="/home" >Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/test">Test</router-link>
+  </nav>
+  <router-view/>
+<!--  确定路由对应的组件渲染的位置-->
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +16,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
