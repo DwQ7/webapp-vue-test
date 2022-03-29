fb43aea28a91e819c6463b3901f165706bc50948
DwQ7 <Qindw1004@@163.com>
2022-03-30 01:03:16 +0800

    修改主页组件

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/images/logo.png">
-    <h2>Welcome to Your Vue.js App</h2>
-    <router-link to="/home/news">News</router-link> |
-    <router-link to="/home/messages">Messages</router-link>
-    <router-view/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <home-content/>
+  <router-view/>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultiData} from "@/network/home";
+import HomeContent from "@/views/home/HomeContent";
+import MainTabBar from "@/components/content/mainTabBar/MainTabBar";
+
 export default {
   name: 'HomeView',
+  data(){
+    return{
+      banner:[],
+      dKeyword:[],
+      keywords:[],
+      recommend:[]
+    }
+  },
   components: {
-    // HelloWorld
+    NavBar,
+    HomeContent,
+    MainTabBar,
+  },
+  created(){
+    console.log(1);
+    getHomeMultiData().then(res =>{
+      console.log(res);
+      this.banner = res.data.banner.list;
+      this.dKeyword = res.data.dKeyword.list;
+      this.keywords = res.data.keywords.list;
+      this.recommend = res.data.recommend.list;
+
+    })
   }
 }
 </script>
@@ -25,5 +45,9 @@ div a{
 }
 div a.router-link-exact-active {
   color: #42b983;
+}
+
+.home_nav{
+  background: #42b983;
 }
 </style>
