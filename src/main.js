1eb7b424779987a3057ab1894a4608aef472f3cb
DwQ7 <Qindw1004@@163.com>
2022-03-30 01:03:57 +0800

    增加element-icon配置

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import 'element-plus/dist/index.css'
+import * as EleIcons from '@element-plus/icons-vue'
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App);
+
+//全局注册图标组件
+for(const name in EleIcons){
+    app.component(name,EleIcons[name])
+}
+
+app.use(router)
+    .use(store)
+    // .use(ElementPlus, { zIndex: 3000, size: 'small' }) // 使用element-plus
+    .mount('#app')
