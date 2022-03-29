52c027c41d771a33e135d2a587e92f5ea5c8ec70
DwQ7 <Qindw1004@@163.com>
2022-03-30 01:07:22 +0800

    添加新组件

---
+++ src/views/home/HomeContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link to="/home/news">News</router-link> |
+  <router-link to="/home/messages">Messages</router-link>
+</template>
+
+<script>
+export default {
+  name: "HomeContent"
+}
+</script>
+
+<style scoped>
+
+</style>
