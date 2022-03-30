126f656d21adc87e13b3c58a8a53fbff7ff1113f
DwQ7 <Qindw1004@@163.com>
2022-03-31 00:38:14 +0800

    主页完成

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,13 +1,25 @@
 <template>
-  <home-content/>
-  <router-view/>
+  <nav-bar class="home_nav"><template #center>桃堡</template></nav-bar>
+  <home-rotation :banner = 'banner'></home-rotation>
+  <home-recommend :recommend = 'recommend'></home-recommend>
+  <home-feature/>
+  <tab-control class="tab_control" :choices="['流行','新款','精选']" ></tab-control>
+  <goods-list :goods="goods"></goods-list>
+
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
-import {getHomeMultiData} from "@/network/home";
-import HomeContent from "@/views/home/HomeContent";
 import MainTabBar from "@/components/content/mainTabBar/MainTabBar";
+
+import {getHomeMultiData,getHomeGoodList} from "@/network/home";
+
+import HomeContent from "@/views/home/childComps/HomeContent";
+import HomeRotation from "@/views/home/childComps/HomeRotation";
+import HomeRecommend from "@/views/home/childComps/HomeRecommend";
+import HomeFeature from "@/views/home/childComps/HomeFeature";
+import TabControl from "@/components/content/tabControl/TabControl";
+import GoodsList from "@/components/content/goods/GoodsList";
 
 export default {
   name: 'HomeView',
@@ -16,38 +28,59 @@ export default {
       banner:[],
       dKeyword:[],
       keywords:[],
-      recommend:[]
+      recommend:[],
+      goods:{
+        'pop':{page:0,list:[]},
+        'new':{page:0,list:[]},
+        'sell':{page:0,list:[]}
+      }
     }
   },
   components: {
     NavBar,
-    HomeContent,
+    TabControl,
     MainTabBar,
+    GoodsList,
+    HomeContent,
+    HomeRotation,
+    HomeRecommend,
+    HomeFeature
   },
   created(){
-    console.log(1);
-    getHomeMultiData().then(res =>{
-      console.log(res);
-      this.banner = res.data.banner.list;
-      this.dKeyword = res.data.dKeyword.list;
-      this.keywords = res.data.keywords.list;
-      this.recommend = res.data.recommend.list;
-
-    })
+    this.getHomeMultiData()
+    let choices = Object.keys(this.goods)
+    for(let i in choices){
+      this.getHomeGoodList(choices[i]);
+    }
+  },
+  methods:{
+    getHomeMultiData(){
+      getHomeMultiData().then(res =>{
+        console.log(res)
+        this.banner = res.data.banner.list
+        this.dKeyword = res.data.dKeyword.list
+        this.keywords = res.data.keywords.list
+        this.recommend = res.data.recommend.list
+      })
+    },
+    getHomeGoodList(type){
+      const page = this.goods[type].page + 1
+      getHomeGoodList(type,page).then(res =>{
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page +=1
+      })
+    }
   }
 }
 </script>
 
 <style>
-div a{
-  font-weight: bold;
-  color: #2c3e50;
+.home_nav{
+  background-color: #FF7A9C;
 }
-div a.router-link-exact-active {
-  color: #42b983;
+.tab_control{
+  position: sticky;
+  top:5vh;
 }
 
-.home_nav{
-  background: #42b983;
-}
 </style>
---
+++ src/views/home/childComps/HomeContent.vue
@@ -0,0 +1,14 @@
+<template>
+<!--  <router-link to="/childComps/news">News</router-link> |-->
+<!--  <router-link to="/childComps/messages">Messages</router-link>-->
+</template>
+
+<script>
+export default {
+  name: "HomeContent"
+}
+</script>
+
+<style scoped>
+
+</style>
