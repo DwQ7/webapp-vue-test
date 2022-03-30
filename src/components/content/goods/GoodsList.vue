bc873c6c7b2f379026c82cd177dae1622a8b0bc8
DwQ7 <Qindw1004@@163.com>
2022-03-31 00:39:16 +0800

    新增商品列表组件

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <goods-list-item v-for="item in updateGoodList" key="item" :good-item="item"></goods-list-item>
+  </div>
+  <div style="text-align: center;color: #666666;line-height: 6vh" >下拉查看更多</div>
+</template>
+
+<script>
+import GoodsListItem from "@/components/content/goods/GoodsListItem";
+
+export default {
+  name: "GoodsList",
+  props:{
+    goods:{
+      type:Object,
+      default(){
+        return {}
+      }
+    },
+    currentIndex:{
+      type:Number,
+      default() {
+        return 0;
+      }
+    }
+  },
+  components:{
+    GoodsListItem
+  },
+  computed:{
+    updateGoodList(){
+        // if(this.$store.getters.getCurrentIndex === 0){
+        //   return this.goods['pop'].list
+        // }else if(this.$store.getters.getCurrentIndex === 1){
+        //   return this.goods['new'].list
+        // }else{
+        //   return this.goods['sell'].list
+        // }
+        switch (this.$store.getters.getCurrentIndex){
+          case 1:
+            return this.goods['pop'].list
+          case 2:
+            return this.goods['new'].list
+          default:
+            return this.goods['sell'].list
+        }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="good_list">
+    <img :src="goodItem.show.img">
+    <div class="good_info">
+      <p>{{goodItem.title}}</p>
+      <span class="price">￥{{goodItem.price}}</span>
+      <el-icon><star /></el-icon>
+      <span class="collect">{{goodItem.cfav}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoodsListItem",
+  props:{
+    goodItem:{
+      type:Object,
+      default() {
+        return {};
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.good_list{
+  float: left;
+  width: 48%;
+  /*position: relative;*/
+  padding: 0 3px 20px 3px;
+}
+.good_list img{
+  width: 100%;
+  height: 270px;
+  border-radius: 5px;
+}
+.good_info{
+  font-size: 15px;
+  overflow: hidden;
+  text-align: center;
+  /*padding-bottom: 30px;*/
+}
+.good_info p{
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.good_info .price{
+  padding-right: 5px;
+  color: var(--body-color);
+}
+
+</style>
